3f2be485b707659179b6d7703ddf50cd048266d7
vivi <a1@1s-MacBook-Pro.local>
2019-03-29 23:43:10 +0200

    bugs_fixed

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let overlay = document.getElementsByClassName('launchpad'),
+    burgerToggle = document.getElementsByClassName('nav__burger');
+
+    burgerToggle.onclick = function() {
+      overlay.style='block';
+    }
